9333e8c469ead77424853239fd6a0566c3a43355
klObs <jw@klobs.de>
2011-03-17 01:03:32 +0100

    Logging: remove outdated print methods from Log.java

--- DC+/src/de/tu/dresden/dud/dc/Log.java
+++ DC+/src/de/tu/dresden/dud/dc/Log.java
@@ -4,45 +4,10 @@
  */
 package de.tu.dresden.dud.dc;
 
-import java.util.Arrays;
+
 import java.util.Observable;
 
-/**
- * @author klobs
- * 
- * This is the logging facility of the service. Each Server can has one.
- * (Singleton)
- * 
- * By now there are following log-verbosities
- * 
- * 0...1 = ERROR
- * 2...3 = WARN
- * 4...5 = INFO
- * 6...9 = DEBUG
- */
 public class Log extends Observable{ // TODO -- Javax.log
-
-	// Loglevel
-	
-	/**
-	 * Log level. Take this log level for logging output that happens on actual errors.
-	 */
-	public static final int LOG_ERROR 	= 1;
-	
-	/**
-	 * Log level. Take this log level for logging output that is a warning. (E.g. fixable errors)
-	 */
-	public static final int LOG_WARN  	= 3;
-	
-	/**
-	 * Log level. Take this log level for logging output that is just for info
-	 */
-	public static final int LOG_INFO	= 5;
-	
-	/**
-	 * Log level. Take this log level for all those useless developer infos...
-	 */
-	public static final int LOG_DEBUG	= 9;
 	
 	// internal variables
 	private int 				currentLogLevel 	= 10;
@@ -50,16 +15,6 @@ public class Log extends Observable{ // TODO -- Javax.log
 	private boolean 			observable			= true;
 		
 	private Log(){}
-	
-	private String getCallerClassName(){
-		Throwable 			t 					= new Throwable(); 
-		StackTraceElement[] elements 			= t.getStackTrace(); 
-		if (elements.length >= 3)
-			return elements[2].getClassName();
-		else 
-			return "";
-	}
-	
 	
 	/**
 	 * This is a singleton, so use this instead of the constructor!
@@ -86,51 +41,6 @@ public class Log extends Observable{ // TODO -- Javax.log
 	 */
 	public boolean observable(){
 		return this.observable;
-	}
-	
-	/**
-	 * make some debug output
-	 * @param logLevel which log level is this message (for different levels, see class description)
-	 * @param l string that has to be logged.
-	 * @param sender sender of the log message
-	 */
-	public static void print(int logLevel, String l, Object sender){
-		if (Log.getInstance().getLogLevel() > 6)
-			System.out.print(Log.getInstance().getCallerClassName() + " ");
-		
-		if (logLevel < Log.getInstance().getLogLevel())
-			System.out.println(l);
-		
-		if (Log.getInstance().observable() == true) {
-			Log.getInstance().setChanged();
-			Log.getInstance().notifyObservers(sender.toString() + "|" + l);
-		}
-	}
-	
-	
-	/**
-	 * make some debug output.
-	 * this is specially usefull for management messages (as they are prepared out of byte-arrays)
-	 * Object s is the sender of the log message;
-	 * @param logLevel
-	 * @param l
-	 * @param b
-	 * @param sender sender of the log message
-	 */
-	public static void print(int logLevel, String l, byte[] b, Object sender){
-		if (Log.getInstance().getLogLevel() > 6)
-			System.out.print(Log.getInstance().getCallerClassName() + " ");	
-		
-		String m = l.concat(Arrays.toString(b));
-		
-		if (logLevel < Log.getInstance().getLogLevel()){
-			System.out.println(m);
-		}
-
-		if (Log.getInstance().observable() == true) {
-			Log.getInstance().setChanged();
-			Log.getInstance().notifyObservers(sender.toString() + "|" + m);
-		}
 	}
 	
 	
